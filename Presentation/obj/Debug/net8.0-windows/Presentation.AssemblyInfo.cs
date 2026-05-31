e90df18d2e627a14c63c261a3e28c8e8c7392332
EAW ACADEMY <oddynucleth@example.com>
2026-05-30 23:00:46 -0600

    Integrantes Grupo

--- Presentation/obj/Debug/net8.0-windows/Presentation.AssemblyInfo.cs
+++ Presentation/obj/Debug/net8.0-windows/Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3351f25ee8238922d58b46aa9940e12dc91321f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a9d7a3e90ead456223232bc9bf1cdcb5b8ed3524")]
 [assembly: System.Reflection.AssemblyProductAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
